332be3defebe9e8002840fe0b0103990b428aa87
SebaGCh <seba.cuentas@gmail.com>
2023-06-15 15:23:56 -0400

    Se agregaron comentarios a cada ejercicio

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+/* Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “Hello World”. (pantalla) */
 alert("Hello World")
 
+
+/* Escribe un programa de una sola línea que escriba en la pantalla un texto que diga “Hello World” (document.write).(pantalla)*/
 document.write("Hello World")
+
+/* Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.(consola)*/
 console.log(3+5)
 
+/* Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga “Hola nombreUsuario”(pantalla) */
 let nombre = prompt("Indica tu nombre")
 alert(`Hola ${nombre}`)
 
+
+/* Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.(consola) */
 let n1 = prompt("Primer Numero")
 let n2 = prompt("Segundo Numero")
 console.log(`La Suma de ${n1} y ${n2} es = ${parseInt(n1) + parseInt(n2)}`)
 
+
+/* Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.(consola) */
 function mayorMenor(numeroX, numeroY){
     if(numeroX > numeroY){
         console.log(`El numero mayor es ${numeroX}`)
@@ -20,15 +30,20 @@ function mayorMenor(numeroX, numeroY){
 mayorMenor(5,3)
 mayorMenor(3,8)
 
+
+/* Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.(consola) */
 function elMayorDeTres(numero1, numero2, numero3){
     console.log(Math.max(numero1, numero2, numero3))
 }
 elMayorDeTres(4, 7, 8)
 
 
+/* Escribe un programa que pida un número y diga si es divisible por 2(consola) */
 var func = (numeroZ) => numeroZ % 2 == 0 ? "Es divisible" : "No es divisible"
 console.log(func(8))
 
+
+/* Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a(pantalla) */
 function EncuentraLetras(cadena){
     let count = 0
     for(let i = 0; i <= cadena.length; i++){
@@ -36,7 +51,6 @@ function EncuentraLetras(cadena){
             count ++
         }
     }
-    alert(`El numero de veces que se repite 'A' en ${cadena} es ${count}`);
+    alert(`El numero de veces que se repite 'A' en "${cadena}" es ${count}`);
 }
-
 EncuentraLetras("Había una vez")
