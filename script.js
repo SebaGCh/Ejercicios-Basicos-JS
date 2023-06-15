4e08b499d3d71804e68ed729fccb365c716a59b6
SebaGCh <seba.cuentas@gmail.com>
2023-06-15 19:33:33 -0400

    Mayor a Menor pasado a ECMA6

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 /* Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “Hello World”. (pantalla) */
-alert("Hello World")
+/* alert("Hello World") */
 
 
 /* Escribe un programa de una sola línea que escriba en la pantalla un texto que diga “Hello World” (document.write).(pantalla)*/
-document.write("Hello World")
+/* document.write("Hello World") */
 
 /* Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.(consola)*/
-console.log(3+5)
+/* console.log(3+5) */
 
 /* Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga “Hola nombreUsuario”(pantalla) */
-let nombre = prompt("Indica tu nombre")
-alert(`Hola ${nombre}`)
+/* let nombre = prompt("Indica tu nombre")
+alert(`Hola ${nombre}`) */
 
 
 /* Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.(consola) */
-let n1 = prompt("Primer Numero")
+/* let n1 = prompt("Primer Numero")
 let n2 = prompt("Segundo Numero")
-console.log(`La Suma de ${n1} y ${n2} es = ${parseInt(n1) + parseInt(n2)}`)
+console.log(`La Suma de ${n1} y ${n2} es = ${parseInt(n1) + parseInt(n2)}`) */
 
 
 /* Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.(consola) */
-function mayorMenor(numeroX, numeroY){
+/* function mayorMenor(numeroX, numeroY){
     if(numeroX > numeroY){
         console.log(`El numero mayor es ${numeroX}`)
     }else{
         console.log(`El Numero ${numeroY} es mayor`)
     }
-}
-mayorMenor(5,3)
+} */
+/* var menorMayor = (numeroX, numeroY) => numeroX > numeroY ? (numeroX + " Es mayor que " + numeroY) : (numeroY + " Es mayor que " + numeroX) 
+console.log(menorMayor(5,8))
+console.log(menorMayor(8,5)) */
+/* mayorMenor(5,3)
 mayorMenor(3,8)
-
+ */
 
 /* Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.(consola) */
-function elMayorDeTres(numero1, numero2, numero3){
+/* function elMayorDeTres(numero1, numero2, numero3){
     console.log(Math.max(numero1, numero2, numero3))
 }
-elMayorDeTres(4, 7, 8)
+elMayorDeTres(4, 7, 8) */
 
 
 /* Escribe un programa que pida un número y diga si es divisible por 2(consola) */
-var func = (numeroZ) => numeroZ % 2 == 0 ? "Es divisible" : "No es divisible"
-console.log(func(8))
+/* var func = (numeroZ) => numeroZ % 2 == 0 ? "Es divisible" : "No es divisible"
+console.log(func(8)) */
 
 
 /* Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a(pantalla) */
